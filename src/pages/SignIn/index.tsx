abb6a867d25e5e1f6674d59319a05f649f31f6cb
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-07 22:50:09 -0300

    Implementa componentes Button e Input na página SignIn.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent } from 'react'
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import logo from './../../assets/logo.svg'
 import { Container, Content, Backgorund } from './styles'
+
+import Button from './../../components/Button'
+import Input from './../../components/Input'
 
 const SignIn: FunctionComponent = () => (
   <Container>
@@ -10,9 +13,14 @@ const SignIn: FunctionComponent = () => (
       <img src={logo} alt="" />
 
       <form>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" placeholder="E-mail" icon={FiMail} />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Senha"
+          icon={FiLock}
+        />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
