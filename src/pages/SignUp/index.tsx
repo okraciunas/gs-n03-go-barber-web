0ac574cc8d04ce143e77b64b1a689aad9a37e8d6
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-09 18:47:46 -0300

    Implementa validação dos campos do form de SignUp.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useCallback } from 'react'
 import { Form } from '@unform/web'
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import * as validation from 'yup'
 
 import logo from './../../assets/logo.svg'
 import { Container, Content, Backgorund } from './styles'
@@ -9,9 +10,26 @@ import Button from './../../components/Button'
 import Input from './../../components/Input'
 
 const SignUp: FunctionComponent = () => {
-  function handleSubmit(data: object): void {
-    console.log(data)
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = validation.object().shape({
+        name: validation.string().required('Nome é um campo obrigatório.'),
+        email: validation
+          .string()
+          .email('Digite um e-mail válido')
+          .required('E-mail é um campo obrigatório'),
+        password: validation
+          .string()
+          .min(6, 'Mínimo de 6 caracteres para a senha.'),
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
   return (
     <Container>
