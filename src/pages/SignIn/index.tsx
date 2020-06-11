d4f55f3b6e07bbc1dde89933dab35e1f122e0944
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-11 19:36:42 -0300

    Adiciona validação no form de login.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useRef, useCallback } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import * as validation from 'yup'
 
 import logo from './../../assets/logo.svg'
 import { Container, Content, Backgorund } from './styles'
@@ -7,32 +10,57 @@ import { Container, Content, Backgorund } from './styles'
 import Button from './../../components/Button'
 import Input from './../../components/Input'
 
-const SignIn: FunctionComponent = () => (
-  <Container>
-    <Content>
-      <img src={logo} alt="GoBarber" />
+import getValidationErrors from './../../utils/get-validation-errors'
 
-      <form>
-        <h1>Faça seu logon</h1>
+const SignIn: FunctionComponent = () => {
+  const formRef = useRef<FormHandles>(null)
 
-        <Input name="email" placeholder="E-mail" icon={FiMail} />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Senha"
-          icon={FiLock}
-        />
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = validation.object().shape({
+        email: validation
+          .string()
+          .email('Digite um e-mail válido')
+          .required('E-mail é um campo obrigatório'),
+        password: validation.string().required('Senha é um campo obrigatório'),
+      })
 
-      <a href="signup">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Backgorund />
-  </Container>
-)
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+    } catch (error) {
+      const errors = getValidationErrors(error)
+      formRef.current?.setErrors(errors)
+    }
+  }, [])
+
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="signup">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Backgorund />
+    </Container>
+  )
+}
 
 export default SignIn
