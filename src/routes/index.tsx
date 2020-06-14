9969875573c0485b9d0f76ef28a4c67164b1a554
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-14 15:28:48 -0300

    Utiliza componente customiza Route.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+
+import Route from './Route'
 
 import SignIn from './../pages/SignIn'
 import SignUp from './../pages/SignUp'
+import Dashboard from './../pages/Dashboard'
 
 const Routes: FunctionComponent = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 )
 
