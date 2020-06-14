f77f9ac7295f1522688823a97695da40bf962865
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-14 12:14:10 -0300

    Altera ícone do Toast de acordo com o tipo.

--- src/components/Toast/Message/index.tsx
+++ src/components/Toast/Message/index.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent } from 'react'
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi'
+import React, { FunctionComponent, useEffect } from 'react'
+import { FiInfo, FiCheckCircle, FiAlertCircle, FiXCircle } from 'react-icons/fi'
 
 import { Container } from './styles'
 
@@ -9,12 +9,28 @@ interface ToastProps {
   data: ToastData
 }
 
+const typeIcon = {
+  info: <FiInfo size={24} />,
+  success: <FiCheckCircle size={24} />,
+  error: <FiAlertCircle size={24} />,
+}
+
 const ToastMessage: FunctionComponent<ToastProps> = ({ data }) => {
   const { removeToast } = useToast()
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(data.id)
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [removeToast, data.id])
+
   return (
     <Container type={data.type} hasMessage={!!data.message}>
-      <FiAlertCircle />
+      {typeIcon[data.type || 'info']}
       <div>
         <strong>{data.title}</strong>
         {data.message && <p>{data.message}</p>}
